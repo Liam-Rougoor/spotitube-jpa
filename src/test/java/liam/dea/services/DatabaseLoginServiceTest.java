9a77e74ca6f1c41351051afe1a22881e16f6e769
Liam-Rougoor <liam.rougoor@hotmail.nl>
2019-03-25 12:45:52 +0100

    DatabaseLoginService Unit Test

--- src/test/java/liam/dea/services/DatabaseLoginServiceTest.java
+++ src/test/java/liam/dea/services/DatabaseLoginServiceTest.java
@@ -1,8 +1,36 @@
 package liam.dea.services;
 
+import liam.dea.dataobjects.User;
+import liam.dea.persistence.TokenDAO;
+import liam.dea.persistence.UserDAO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class DatabaseLoginServiceTest {
-    
 
+    @Mock
+    private UserDAO userDAOStub;
+
+    @Mock
+    private TokenDAO tokenDAOMock;
+
+
+    @InjectMocks
+    private DatabaseLoginService systemUnderTest;
+
+    @Test
+    void callsGetLogin() {
+        User user = new User();
+        user.setUser("liam");
+        Mockito.when(userDAOStub.getUserByNameAndPassword("liam", "pass")).thenReturn(user);
+        systemUnderTest.getLogin("liam","pass");
+        Mockito.verify(tokenDAOMock).getLogin("liam");
+    }
 }
